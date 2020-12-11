3de96a7d33f951b628ae877295b39ee6ea3f5d37
Volta <giovoltadesign@gmail.com>
2020-12-10 23:21:55 -0300

    module refactor

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
+import { printCard, getRandomInt } from './module.js'
+
 document.addEventListener('DOMContentLoaded', () => {
   const random = getRandomInt(1, 150)
   fetchApi(random)
 })
 
-const getRandomInt = (min, max) =>  {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+
 
 const fetchApi = async (id) => {
   try {
@@ -28,19 +28,4 @@ const fetchApi = async (id) => {
   }
 }
 
-const printCard = (pokemon) => {
-  const contain = window.contain
-  const template = window['template-card'].content
-  const clone = template.cloneNode(true)
-  const fragment = document.createDocumentFragment()
-
-  clone.querySelector('.card__image').setAttribute('src', pokemon.img)
-  clone.querySelector('.card__title').innerHTML = `${pokemon.name} <span>${pokemon.hp} hp</span>`
-  clone.querySelector('.card__exp').textContent = `${pokemon.experience} Exp`
-  clone.querySelectorAll('.card__social--prop h3')[0].textContent = pokemon.attack
-  clone.querySelectorAll('.card__social--prop h3')[1].textContent = pokemon.special
-  clone.querySelectorAll('.card__social--prop h3')[2].textContent = pokemon.defense
-  fragment.appendChild(clone)
-  contain.appendChild(fragment)
-}
 
